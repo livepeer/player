33722523953841540c9410d8d3735af2a9562fe7
Victor Elias <victorgelias@gmail.com>
2022-09-30 18:13:12 -0300

    Upgrade player for new design & controls (#12)
    * Update dependencies
    
    * Update the player api
    
    * Remove that theme
    
    * Remove parent div altogether
    
    * Revert "Remove parent div altogether"
    
    This reverts commit 59255464001a24cdfec815a021d254def1bfdf45.
    
    * Update @livepeer/react
    
    * Simplify player usage
    
    Fixes controls!
    
    * Fit video content to the iframe area
    
    * Respect autoplay/muted requests instead of defaults
    
    ?muted=0 would start autoplaying muted cause we forced
    autoplay to be 1.
    
    * Use transparent background when within iframe
    
    * Add aspectRatio query string option
    
    * Log isIframe errors
    
    * Revert "Log isIframe errors"
    
    This reverts commit eb4645e62fcb994e0df5ab3844e668047b5f824f.
    
    * Fix conditional background color
    
    * Make object fit configurable through querystring as well
    
    * Upgrade @livepeer/react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { useMemo } from 'react'
-
-import { VideoPlayer } from '@livepeer/react'
-
-import styles from '../styles/Player.module.css'
+import { useEffect, useMemo } from 'react'
+import { AspectRatio, Player } from '@livepeer/react'
 
 const isTrue = (b: string) =>
   b === '' || b === '1' || b?.toLowerCase() === 'true'
@@ -17,23 +14,68 @@ function toStringValues(obj: Record<string, any>) {
   return strObj
 }
 
-const Player: NextPage = () => {
+function isIframe() {
+  try {
+    return window.self !== window.top
+  } catch {}
+  try {
+    return window.self !== window.parent
+  } catch {}
+  // default to true as this is only used to set a transparent background
+  return true
+}
+
+const PlayerPage: NextPage = () => {
   const { query: rawQuery } = useRouter()
   const query = useMemo(() => toStringValues(rawQuery), [rawQuery])
-  const { autoplay = '1', muted = autoplay, loop, v, url } = query
+  let {
+    autoplay,
+    muted,
+    loop,
+    aspectRatio = '16to9',
+    objectFit = 'contain',
+    v,
+    url,
+  } = query
+  if (autoplay === undefined && (muted === undefined || isTrue(muted))) {
+    autoplay = muted = '1'
+  }
 
-  const srcProps = url ? { src: url } : { playbackId: v }
+  useEffect(() => {
+    if (!isIframe()) {
+      document.body.style.backgroundColor = 'black'
+    }
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <VideoPlayer
-        className={styles.player}
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+      }}
+    >
+      <Player
+        src={url}
+        playbackId={v}
         muted={isTrue(muted)}
         autoPlay={isTrue(autoplay)}
         loop={isTrue(loop)}
-        {...srcProps}
+        objectFit={objectFit as any}
+        aspectRatio={aspectRatio as AspectRatio}
+        theme={{
+          radii: {
+            containerBorderRadius: '0px',
+          },
+        }}
       />
     </div>
   )
 }
 
-export default Player
+export default PlayerPage
