91fdefe5de428a2feadc8feab8b862d20b333f46
Victor Elias <victorgelias@gmail.com>
2022-09-30 13:08:04 -0300

    Simplify player usage
    Fixes controls!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useMemo } from 'react'
-
 import { Player } from '@livepeer/react'
-
-import styles from '../styles/Player.module.css'
 
 const isTrue = (b: string) =>
   b === '' || b === '1' || b?.toLowerCase() === 'true'
@@ -17,22 +14,39 @@ function toStringValues(obj: Record<string, any>) {
   return strObj
 }
 
-const EmbeddablePlayer: NextPage = () => {
+const PlayerPage: NextPage = () => {
   const { query: rawQuery } = useRouter()
   const query = useMemo(() => toStringValues(rawQuery), [rawQuery])
   const { autoplay = '1', muted = autoplay, loop, v, url } = query
 
-  const srcProps = url ? { src: url } : { playbackId: v }
   return (
-    <div className={styles.container}>
+    <div
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        backgroundColor: '#000',
+      }}
+    >
       <Player
+        src={url}
+        playbackId={v}
         muted={isTrue(muted)}
-        autoPlay={isTrue(autoplay) as any}
+        autoPlay={isTrue(autoplay)}
         loop={isTrue(loop)}
-        {...srcProps}
+        theme={{
+          radii: {
+            containerBorderRadius: '0px',
+          },
+        }}
       />
     </div>
   )
 }
 
-export default EmbeddablePlayer
+export default PlayerPage
