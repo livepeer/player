e91e663bd89384d51e38ab6b2a0b2e07f02670f9
Chase Adams <c@cadams.io>
2022-09-30 17:16:03 -0700

    fix: fix custom storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,36 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 import {
   LivepeerConfig,
   createReactClient,
   studioProvider,
-} from '@livepeer/react'
-import memoryLocalStorage from 'localstorage-memory'
-import '../styles/globals.css'
+  noopStorage,
+  createStorage,
+} from "@livepeer/react";
+import "../styles/globals.css";
 
 const isStaging =
-  typeof window !== 'undefined' &&
-  new URLSearchParams(window.location.search).has('monster')
+  typeof window !== "undefined" &&
+  new URLSearchParams(window.location.search).has("monster");
 const livepeer = createReactClient({
-  storage: memoryLocalStorage,
+  storage: createStorage({
+    storage: noopStorage
+  }),
   provider: studioProvider(
     !isStaging
       ? undefined
       : {
-          baseUrl: 'https://livepeer.monster/api',
-          apiKey: '',
+          baseUrl: "https://livepeer.monster/api",
+          apiKey: "",
         }
   ),
-})
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LivepeerConfig client={livepeer}>
       <Component {...pageProps} />
     </LivepeerConfig>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
